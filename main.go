9208c28e4fc3c7628778fa40cc42663088dc3e3c
Madsakra <hongjun820@gmail.com>
2025-05-22 11:23:02 +0800

    storage1

--- main.go
+++ main.go
@@ -1,22 +1,38 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"example.com/m/v2/internal/database"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	DB             *database.Queries
 }
 
 func main() {
+
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	dbQueries := database.New(db)
 
 	const filepathRoot = "."
 	const port = "8080"
 
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
+		DB:             dbQueries,
 	}
 
 	mux := http.NewServeMux()
